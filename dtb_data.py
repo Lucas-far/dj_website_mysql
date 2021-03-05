0587f21f97a564f395919f7f01403de3493f65aa
Lucas-far <mizoguchia@hotmail.com>
2021-03-05 12:53:30 -0300

    configure postgresql

---
+++ dtb_data.py
@@ -0,0 +1,18 @@
+
+
+def show_dtb_name():
+    return 'database_postgresql_1st'
+
+
+def show_dtb_user():
+    return 'user_postgresql_1st'
+
+
+def show_dtb_password():
+    return 'wirsindbleiben0727'
+
+
+if __name__ == '__main__':
+    print(show_dtb_name())
+    print(show_dtb_user())
+    print(show_dtb_password())
--- pp/settings.py
+++ pp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from dtb_data import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'pp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': f'{show_dtb_name()}',
+        'USER': f'{show_dtb_user()}',
+        'PASSWORD': f'{show_dtb_password()}',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -120,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# ------------------------------------------------- DADOS ADICIONADOS --------------------------------------------------
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
